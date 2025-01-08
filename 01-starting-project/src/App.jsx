081bf00116df82d64ad7075291c2dbad5f9700fa
busracogul <cogulbusra@gmail.com>
2025-01-08 16:34:45 +0300

    01-starting-project

---
+++ 01-starting-project/src/App.jsx
@@ -0,0 +1,43 @@
+import { CORE_CONCEPTS } from "./data";
+
+function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt="" />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          Fundamental React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              image={CORE_CONCEPTS[0].image}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
